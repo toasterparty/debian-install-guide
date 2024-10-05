007bf240e5bae6fb3f8f6fa153f0674726a02f59
toasterparty <toasterparty@derpymail.org>
2024-10-05 13:14:36 -0700

    WIP

--- sh/update.sh
+++ sh/update.sh
@@ -6,15 +6,14 @@ SCRIPT_PATH=$(readlink -f "$0")
 
 # cron.sh <job-name> <command> <frequency>
 # (every monday at 3am)
-# $SCRIPT_DIR/cron.sh "update" "$SCRIPT_PATH" "0 3 * * 1"
-echo $SCRIPT_DIR/cron.sh "update" "$SCRIPT_PATH" "0 3 * * 1"
+$SCRIPT_DIR/cron.sh "update" "$SCRIPT_PATH" "0 3 * * 1"
 
 echo "Update system..."
-sudo -qq apt-get update -y --allow-releaseinfo-change
-sudo -qq apt-get --fix-broken install
+sudo apt-get -qq update -y --allow-releaseinfo-change
+sudo apt-get -qq --fix-broken install
 sudo dpkg --configure -a
-sudo -qq apt full-upgrade -y
-sudo -qq apt-get clean -y
-sudo -qq apt-get --purge autoremove -y
-sudo -qq apt-get autoclean -y
+sudo apt-get -qq full-upgrade -y
+sudo apt-get -qq clean -y
+sudo apt-get -qq --purge autoremove -y
+sudo apt-get -qq autoclean -y
 echo "System update complete"
