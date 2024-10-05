650df222a1f1887ff8e49864e3cf12131739d97f
toasterparty <toasterparty@derpymail.org>
2024-10-05 15:39:54 -0700

    WIP

--- configure.sh
+++ configure.sh
@@ -2,25 +2,159 @@
 set -e
 
 HOST="https://toasterparty.github.io/debian-setup-guide"
+SH="$HOME/sh"
 
-# Enable Passwordless sudo
-
-LINE='%sudo ALL=(ALL) NOPASSWD: ALL'
-FILEPATH='/etc/sudoers'
-sudo grep -xsqF "$LINE" "$FILEPATH" || echo "$LINE" | sudo tee -a "$FILEPATH"
-echo "Passwordless Sudo: OK"
-
-# Download util script
+# Download util scripts
 
 FILENAMES=("update.sh" "cron.sh")
-mkdir -p $HOME/sh && cd $HOME/sh
+mkdir -p $SH && cd $SH
 for FILENAME in "${FILENAMES[@]}"; do
-    FILEPATH=$HOME/sh/$FILENAME
-    wget -nv -N $HOST/sh/$FILENAME
+    FILEPATH=$SH/$FILENAME
+    wget -nv -N $HOST/$FILENAME
     chmod +x $FILEPATH
-    echo "~/sh/$FILENAME OK"
 done
 
-# Initial Update
+### CONFIG MENU ###
 
-$HOME/sh/update.sh
+show_menu() {
+    echo ""
+    echo "Choose an option:"
+    echo "1) Enable passwordless sudo"
+    echo "2) Update packages"
+    echo "3) Install common packages"
+    echo "4) Update firmware"
+    echo "5) Enable weekly system update"
+
+    # docker
+    # git
+    # ssh
+
+    echo "98) ALL OF THE ABOVE"
+    echo "99) Exit"
+}
+
+while true; do
+    show_menu
+    read -p ">" CHOICE
+    case "$CHOICE" in
+        1)
+            # Enable passwordless sudo
+            LINE='%sudo ALL=(ALL) NOPASSWD: ALL'
+            FILEPATH='/etc/sudoers'
+            sudo grep -xsqF "$LINE" "$FILEPATH" || echo "$LINE" | sudo tee -a "$FILEPATH"
+            echo "Passwordless sudo: OK"
+            ;;
+        2)
+            # Update packages
+            $SH/update.sh
+            ;;
+        3)
+            # Install common packages
+            SYS_PKG="ufw ca-certificates gnupg"
+            UTIL_PKG="wget curl openssh-server"
+            DEV_PKG="git cmake ccache docker"
+            PYTHON_PKG="python3 python3-venv python3-setuptools python3-pip"
+            sudo apt-get install -m -y $SYS_PKG $UTIL_PKG $DEV_PKG $PYTHON_PKG
+            echo "Common packages installation complete"
+            ;;
+        4)
+            # Update firmware
+
+            # Add firmware to apt sources
+            SOURCES_LIST="/etc/apt/sources.list"
+            TAGS=("contrib" "non-free" "non-free-firmware")
+            for TAG in "${TAGS[@]}"; do
+                sudo sed -i "/main/ { / $TAG$/! s/$/ $TAG/ }" $SOURCES_LIST
+            done
+
+            $SH/update.sh
+            sudo apt-get install -m -y fwupd firmware-linux-nonfree
+
+            # Reload fwupd service to ensure it's up-to-date
+            sudo systemctl daemon-reload
+            sudo systemctl restart fwupd
+
+            # Refresh the list of available firmware updates
+            echo "Checking for firmware updates..."
+            sudo fwupdmgr refresh --force
+
+            # Check for available updates
+            sudo fwupdmgr get-updates
+            sudo fwupdmgr update
+
+            echo "Done checking for firmware updates. A reboot may or may not be nececssary"
+            ;;
+        5)
+            # Enable weekly system update
+            $HOME/sh/cron.sh "update" "$HOME/sh/update.sh" "0 3 * * 1"
+            echo System updates will be installed every Monday at 3am
+            ;;
+        99)
+            # Install common packages
+            ;;
+        99)
+            echo "Exiting..."
+            exit 0
+            ;;
+        *)
+            echo "Invalid choice, please try again."
+            ;;
+    esac
+done
+
+# Install docker
+
+# if ! command -v docker &> /dev/null || ! sudo docker info &> /dev/null; then
+#     # Install Docker GPG key
+#     sudo install -m 0755 -d /etc/apt/keyrings
+#     curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
+
+#     # Add Docker repository
+#     echo \
+#       "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+#       $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+#       sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+#     # Update apt and install Docker packages
+#     sudo apt-get update
+#     sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    
+#     # run a little test
+#     sudo docker run hello-world
+
+#     echo "Docker: OK"
+# else
+#     echo "Docker: OK"
+# fi
+
+# Configure git
+
+# Configure SSH
+# ./sh/install.sh openssh-server ufw
+# sudo ufw allow ssh -y
+# sudo systemctl enable ssh --now
+# - Generate an ed25519 key on the client machine if needed
+# - Install vscode Remove/SSH extension(s) if needed
+# - Edit vscode ssh config to include 
+# something like:
+# ```
+# # Server
+# Host odroid-h3p
+#     HostName 192.168.0.186
+#     User toaster
+#     AddKeysToAgent yes
+#     IdentityFile ~/.ssh/id_ed25519
+# ```
+# - Copy the contents of `.ssh/id_ed25519.pub` to the clipboard of the client machine
+# - Connect to server via SSH outside of vscode and edit `~/.ssh/authorized_keys` to include the above public key
+# - Run pallet command `Remote-SSH: Connect to Host…` and follow instructions for first time connection
+
+# Uninstall GUI
+# sudo systemctl set-default multi-user.target
+# sudo systemctl stop gdm3
+# sudo systemctl disable gdm3
+# sudo apt-get remove -y --purge gnome-core
+# sudo apt-get remove -y --purge kde-plasma-desktop
+# sudo apt-get remove -y --purge xfce4
+# sudo apt-get remove -y --purge lxde
+# echo Reboot with "sudo reboot" to apply changes
--- sh/update.sh
+++ sh/update.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
-
-# Idempotently add a cron job to do this every Monday at 3am
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-SCRIPT_PATH=$(readlink -f "$0")
-
-# cron.sh <job-name> <command> <frequency>
-# (every monday at 3am)
-$SCRIPT_DIR/cron.sh "update" "$SCRIPT_PATH" "0 3 * * 1"
-
 echo "Update system..."
 sudo apt-get -qq update -y --allow-releaseinfo-change
 sudo apt-get -qq --fix-broken install
